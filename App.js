34092323338482689ba70606e8b04e4423cf50f1
somenody <117578473+Somenody@users.noreply.github.com>
2023-07-23 20:49:44 +0200

    Version 2.0 with commented code and better package.json files

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+// The need module for start fine
 const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(`
+   // start of the HTML code  
+res.end(`
     <!DOCTYPE html>
     <html>
     <head>
